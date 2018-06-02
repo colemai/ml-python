e6595846bc6c1ba040e6425b8d3d3251e8afcca5
colemai <colemai@tcd.ie>
2018-06-02 09:51:20 +0200

    Added tensorflow linreg and numpy reference notebook

--- tf-lin-reg-grad-desc.py
+++ tf-lin-reg-grad-desc.py
@@ -55,3 +55,46 @@ if __name__ == "__main__":
 	train_y = l[rnd_indices]
 	test_x = f[~rnd_indices]
 	test_y = l[~rnd_indices]
+
+	#Add the data to TF's own data structures
+	learning_rate = 0.01
+	training_epochs = 1000
+	cost_history = np.empty(shape=[1],dtype=float)
+
+	X = tf.placeholder(tf.float32,[None,n_dim])
+	Y = tf.placeholder(tf.float32,[None,1])
+	W = tf.Variable(tf.ones([n_dim,1]))
+
+	init = tf.initialize_all_variables()
+
+	y_ = tf.matmul(X, W) #multiply features matrix to weights matrix
+	cost = tf.reduce_mean(tf.square(y_ - Y)) #mse
+	training_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost) #grad desc optimisation
+
+	#Run training
+	sess = tf.Session()
+	sess.run(init)
+
+	for epoch in range(training_epochs):
+	    sess.run(training_step,feed_dict={X:train_x,Y:train_y})
+	    cost_history = np.append(cost_history,sess.run(cost,feed_dict={X: train_x,Y: train_y}))
+
+
+	#Plot Cost
+
+	plt.plot(range(len(cost_history)),cost_history)
+	plt.axis([0,training_epochs,0,np.max(cost_history)])
+	plt.show()
+
+
+	# preds and mse
+	pred_y = sess.run(y_, feed_dict={X: test_x})
+	mse = tf.reduce_mean(tf.square(pred_y - test_y))
+	print("MSE: %.4f" % sess.run(mse)) 
+
+	fig, ax = plt.subplots()
+	ax.scatter(test_y, pred_y)
+	ax.plot([test_y.min(), test_y.max()], [test_y.min(), test_y.max()], 'k--', lw=3)
+	ax.set_xlabel('Measured')
+	ax.set_ylabel('Predicted')
+	plt.show()
